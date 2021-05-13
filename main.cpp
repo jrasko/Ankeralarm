84909e1647118b75ef3584c7d6eea92db99c6356
jrasko <j.raskob@tarent.de>
2021-05-13 10:09:44 +0200

    Removed Commented AlarmMode On

--- main.cpp
+++ main.cpp
@@ -193,28 +193,6 @@ void loop() {
 	} else {
 		//print no GPS
 	}
-
-	//GPS angeschaltet
-//    while (/* alarmmode */ random() < 0.5) {
-//        vector<Position> posCollection;
-//
-//        for (int i = 0; i < 4; ++i) {
-//            updateGPSData();
-//            posCollection.push_back(a.props.myGPS.getCurrentPosition());
-//        }
-//        const Position startPosition = getMedian(posCollection);
-//
-//        //Radius einstellen
-//        unsigned int radius = 100;
-//
-//        // Abstand zur Ursprungsposition testen.
-//        while (random() < 0.5) {
-//            updateGPSData();
-//            if (startPosition.distanceTo(a.props.myGPS.getCurrentPosition()) > radius) {
-//                alarm();
-//            }
-//        }
-//    }
 }
 
 
